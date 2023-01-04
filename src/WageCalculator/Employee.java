f3443f5d56e63e9c8afdd044ef570da70cb4a820
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-04 14:44:47 -0600

    uses abstract methods in constrctor

--- src/WageCalculator/Employee.java
+++ src/WageCalculator/Employee.java
@@ -4,27 +4,32 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
+    public Employee(int baseSalary, int hourlyRate) {
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
+
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
     }
 
-    public void setBaseSalary(int baseSalary) {
+    private void setBaseSalary(int baseSalary) {
         if (baseSalary <= 0)
             throw new IllegalArgumentException("Salary cannot be 0 or less.");
         this.baseSalary = baseSalary;
     }
 
-    public void setHourlyRate(int hourlyRate) {
+    private void setHourlyRate(int hourlyRate) {
         if (hourlyRate <= 0)
             throw new IllegalArgumentException("Hourly rate cannot be 0 or negative.");
             this.hourlyRate = hourlyRate;
     }
 
-    public int getBaseSalary() {
+    private int getBaseSalary() {
         return baseSalary;
     }
 
-    public int getHourlyRate() {
+    private int getHourlyRate() {
         return hourlyRate;
     }
 }
--- src/WageCalculator/wageCalculator.java
+++ src/WageCalculator/wageCalculator.java
@@ -2,9 +2,8 @@ package WageCalculator;
 
 public class wageCalculator {
     public static void main(String[] args) {
-        var employee = new Employee();
-        employee.setBaseSalary(-1);
-        employee.setHourlyRate(20);
+        var employee = new Employee(
+                50_000, 20);
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
