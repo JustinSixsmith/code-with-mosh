bcd43badf55bb8f9cee3110a8695e6b170602b78
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-05 13:21:52 -0600

    creates point class and overrides equals and hashCode methods

--- src/com/uicontrol/UIMain.java
+++ src/com/uicontrol/UIMain.java
@@ -3,16 +3,9 @@ package com.uicontrol;
 public class UIMain {
 
     public static void main(String[] args) {
-        var control = new UIControl(true);
-        var texBox = new TextBox();
-        show(texBox);
-    }
-
-    public static void show(UIControl control) {
-        if (control instanceof TextBox) {
-            var textBox = (TextBox) control;
-            textBox.setText("Hello World");
-        }
-        System.out.println(control);
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1, 2);
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
     }
 }
