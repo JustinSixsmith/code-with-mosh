94822dd047f61303092e31b36382b02eeee1052e
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-05 12:59:48 -0600

    ui control main works with upcasting and downcasting

--- src/com/uicontrol/UIMain.java
+++ src/com/uicontrol/UIMain.java
@@ -3,9 +3,16 @@ package com.uicontrol;
 public class UIMain {
 
     public static void main(String[] args) {
-        var control = new UIControl();
-        control.disable();
-        System.out.println(control.isEnabled());
+        var control = new UIControl(true);
+        var texBox = new TextBox();
+        show(texBox);
     }
 
+    public static void show(UIControl control) {
+        if (control instanceof TextBox) {
+            var textBox = (TextBox) control;
+            textBox.setText("Hello World");
+        }
+        System.out.println(control);
+    }
 }
