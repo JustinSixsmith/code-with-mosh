0b68a761200e8e90f84c0d9259b34ce9045a96da
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-04 18:29:56 -0600

    overloads employee constructor

--- src/WageCalculator/Employee.java
+++ src/WageCalculator/Employee.java
@@ -3,14 +3,28 @@ package WageCalculator;
 public class Employee {
     private int baseSalary;
     private int hourlyRate;
+    private static int numberOfEmployees;
+
+    public Employee(int baseSalary) {
+        this(baseSalary, 0);
+    }
 
     public Employee(int baseSalary, int hourlyRate) {
         setBaseSalary(baseSalary);
         setHourlyRate(hourlyRate);
+        numberOfEmployees++;
+    }
+
+    public static void printNumberOfEmployees() {
+        System.out.println(numberOfEmployees);
     }
 
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public int calculateWage() {
+        return calculateWage(0);
     }
 
     private void setBaseSalary(int baseSalary) {
@@ -20,7 +34,7 @@ public class Employee {
     }
 
     private void setHourlyRate(int hourlyRate) {
-        if (hourlyRate <= 0)
+        if (hourlyRate < 0)
             throw new IllegalArgumentException("Hourly rate cannot be 0 or negative.");
             this.hourlyRate = hourlyRate;
     }
--- src/WageCalculator/wageCalculator.java
+++ src/WageCalculator/wageCalculator.java
@@ -2,8 +2,8 @@ package WageCalculator;
 
 public class wageCalculator {
     public static void main(String[] args) {
-        var employee = new Employee(
-                50_000, 20);
+        var employee = new Employee(50_000, 20);
+        Employee.printNumberOfEmployees();
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
