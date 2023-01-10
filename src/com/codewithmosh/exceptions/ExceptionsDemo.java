df7ba64365a23479fc9d16616b2b02669008249d
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-10 15:22:57 -0600

    catches multiple exceptions

--- src/com/codewithmosh/exceptions/ExceptionsDemo.java
+++ src/com/codewithmosh/exceptions/ExceptionsDemo.java
@@ -2,14 +2,18 @@ package com.codewithmosh.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
         try {
             var reader = new FileReader("file.txt");
-            System.out.println("File opened");
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+            var value = reader.read();
+            new SimpleDateFormat().parse("");
+        } catch (IOException | ParseException e){
+            System.out.println("Could not read data.");
         }
     }
 
