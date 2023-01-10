6c672ecb79553ca8f5dc9859423cc0c82c0c12af
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-10 14:38:14 -0600

    handling file not found exception

--- src/com/codewithmosh/exceptions/ExceptionsDemo.java
+++ src/com/codewithmosh/exceptions/ExceptionsDemo.java
@@ -1,8 +1,16 @@
 package com.codewithmosh.exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionsDemo {
     public static void show() {
-        sayHello(null);
+        try {
+            var reader = new FileReader("file.txt");
+            System.out.println("File opened");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void sayHello(String name) {
