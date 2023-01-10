f3321ae0ca707981806dacf5e8063496fb66c46e
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-10 15:32:13 -0600

    switches to a try with resources statement

--- src/com/codewithmosh/exceptions/ExceptionsDemo.java
+++ src/com/codewithmosh/exceptions/ExceptionsDemo.java
@@ -8,24 +8,11 @@ import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
 
-        try {
-            reader = new FileReader("file.txt");
+        try (var reader = new FileReader("file.txt")) {
             var value = reader.read();
         } catch (IOException e) {
             System.out.println("Could not read data.");
-        } finally {
-            if (reader != null)
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
         }
-    }
-
-    public static void sayHello(String name) {
-        System.out.println(name.toUpperCase());
     }
 }
