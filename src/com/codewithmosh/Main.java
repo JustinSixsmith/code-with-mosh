0e77667cc80083601b5fc52a65daf5d5f17365b8
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-12 12:27:44 -0600

    creates List, User, and UserList classes

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,15 +1,13 @@
 package com.codewithmosh;
 
 import com.codewithmosh.exceptions.ExceptionsDemo;
+import com.codewithmosh.generics.List;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            ExceptionsDemo.show();
-        } catch (IOException e) {
-            System.out.println("An unexpected error occurred.");
-        }
+        var list = new List();
+        list.add(1);
     }
 }
