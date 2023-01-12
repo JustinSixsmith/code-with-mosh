f3f37a7d04e5776fdf04525a332ce68f47fab90b
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-12 10:06:48 -0600

    adds file writer to try statement

--- src/com/codewithmosh/exceptions/ExceptionsDemo.java
+++ src/com/codewithmosh/exceptions/ExceptionsDemo.java
@@ -2,6 +2,7 @@ package com.codewithmosh.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -9,7 +10,10 @@ import java.text.SimpleDateFormat;
 public class ExceptionsDemo {
     public static void show() {
 
-        try (var reader = new FileReader("file.txt")) {
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("...")
+                ) {
             var value = reader.read();
         } catch (IOException e) {
             System.out.println("Could not read data.");
