96957efed5cef272ffda0c225e5a3095748d9e7c
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-12 14:12:06 -0600

    add Utils class with max static method and toString points

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,18 +4,13 @@ import com.codewithmosh.exceptions.ExceptionsDemo;
 import com.codewithmosh.generics.GenericList;
 import com.codewithmosh.generics.List;
 import com.codewithmosh.generics.User;
+import com.codewithmosh.generics.Utils;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        var user1 = new User(10);
-        var user2 = new User(20);
-        if (user1.compareTo(user2) < 0)
-            System.out.println("user1 < user2");
-        else if (user1.compareTo(user2) == 0)
-            System.out.println("user1 == user2");
-        else
-            System.out.println("user1 > user2");
+        var max = Utils.max(new User(10), new User(20));
+        System.out.println(max);
     }
 }
---
+++ src/com/codewithmosh/generics/Utils.java
@@ -0,0 +1,7 @@
+package com.codewithmosh.generics;
+
+public class Utils {
+    public static <T extends Comparable<T>> T max(T first, T second) {
+        return first.compareTo(second) > 0 ? first : second;
+    }
+}
