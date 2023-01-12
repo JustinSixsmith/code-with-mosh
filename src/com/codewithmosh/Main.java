b3f2f3d8ba5bd5353dcf17e67c694c0cdb0431c7
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-12 13:58:08 -0600

    user class compares points

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -9,8 +9,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new GenericList<User>();
-        list.add(new User());
-        User user = list.get(0);
+        var user1 = new User(10);
+        var user2 = new User(20);
+        if (user1.compareTo(user2) < 0)
+            System.out.println("user1 < user2");
+        else if (user1.compareTo(user2) == 0)
+            System.out.println("user1 == user2");
+        else
+            System.out.println("user1 > user2");
     }
 }
