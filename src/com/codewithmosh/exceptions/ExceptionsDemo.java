e74d328ea2a0e389edb08460e4587f848087a805
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-12 10:14:56 -0600

    adds Account class that can throw and IOException

--- src/com/codewithmosh/exceptions/ExceptionsDemo.java
+++ src/com/codewithmosh/exceptions/ExceptionsDemo.java
@@ -9,14 +9,11 @@ import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
-
-        try (
-                var reader = new FileReader("file.txt");
-                var writer = new FileWriter("...")
-                ) {
-            var value = reader.read();
+        var account = new Account();
+        try {
+            account.deposit(1);
         } catch (IOException e) {
-            System.out.println("Could not read data.");
+            e.printStackTrace();
         }
     }
 }
