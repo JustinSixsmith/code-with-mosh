7e502528b9b0efce4933e1b67315b1a7e87f6552
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-12 12:41:07 -0600

    creates a GenericList class

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,13 +1,16 @@
 package com.codewithmosh;
 
 import com.codewithmosh.exceptions.ExceptionsDemo;
+import com.codewithmosh.generics.GenericList;
 import com.codewithmosh.generics.List;
+import com.codewithmosh.generics.User;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new List();
-        list.add(1);
+        var list = new GenericList<User>();
+        list.add(new User());
+        User user = list.get(0);
     }
 }
