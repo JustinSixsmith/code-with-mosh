9c6158fdc3c59e3f781328ec4dadc723268d4c3a
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-12 11:44:41 -0600

    creates an empty generics package

--- src/com/codewithmosh/exceptions/ExceptionsDemo.java
+++ src/com/codewithmosh/exceptions/ExceptionsDemo.java
@@ -1,11 +1,6 @@
 package com.codewithmosh.exceptions;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() throws IOException {
