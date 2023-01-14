38b32fb578f6c4a1d4db4e7b86a2f739e67de7c5
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-14 10:07:03 -0600

    switches to foreach loops

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -8,10 +8,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         var list = new GenericList<String>();
-        var iterator = list.iterator();
-        while (iterator.hasNext()){
-            var current = iterator.next();
-            System.out.println(current);
+        for (var item : list) {
+            System.out.println(item);
         }
     }
 }
