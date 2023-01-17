17a48640e749d1700435ecb183435bfb17f4fe2d
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-17 05:56:06 -0600

    creates collection class and show method

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,5 +1,6 @@
 package com.codewithmosh;
 
+import com.codewithmosh.collections.CollectionsDemo;
 import com.codewithmosh.exceptions.ExceptionsDemo;
 import com.codewithmosh.generics.*;
 
@@ -7,11 +8,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new GenericList<String>();
-        list.add("a");
-        list.add("b");
-        for (var item : list) {
-            System.out.println(item);
-        }
+        CollectionsDemo.show();
     }
 }
