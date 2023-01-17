d43bcde4f43a67ae48835d8f141ae52c3e8d5558
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-17 09:55:30 -0600

    creates QueueDemo

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,24 +1,9 @@
 package com.codewithmosh;
 
-import com.codewithmosh.collections.CollectionsDemo;
-import com.codewithmosh.collections.Customer;
-import com.codewithmosh.collections.EmailComparator;
-import com.codewithmosh.collections.ListDemo;
-import com.codewithmosh.exceptions.ExceptionsDemo;
-import com.codewithmosh.generics.*;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import com.codewithmosh.collections.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Customer> customers = new ArrayList<>();
-        customers.add(new Customer("b", "e3"));
-        customers.add(new Customer("a", "e2"));
-        customers.add(new Customer("c", "e1"));
-        Collections.sort(customers, new EmailComparator());
-        System.out.println(customers);
+        QueueDemo.show();
     }
 }
---
+++ src/com/codewithmosh/collections/QueueDemo.java
@@ -0,0 +1,15 @@
+package com.codewithmosh.collections;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void show() {
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add("c");
+        queue.add("a");
+        queue.add("b");
+        var front = queue.poll();
+        System.out.println(front);
+    }
+}
