ae4faeb0f7b964863577e492914bccd48b5c676a
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-17 14:38:33 -0600

    combines BinaryOperator with square Function

--- src/com/codewithmosh/lambdas/LambdasDemo.java
+++ src/com/codewithmosh/lambdas/LambdasDemo.java
@@ -1,14 +1,14 @@
 package com.codewithmosh.lambdas;
 
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class LambdasDemo {
     public static void show() {
-        Predicate<String> hasLeftBrace = str -> str.startsWith("{");
-        Predicate<String> hasRightBrace = str -> str.endsWith("}");
-
-        var result = hasLeftBrace.negate().test("key:value}");
+        BinaryOperator<Integer> add = (a, b) -> a + b;
+        Function<Integer, Integer> square = a -> a * a;
+        var result = add.andThen(square).apply(1, 2);
         System.out.println(result);
     }
 }
