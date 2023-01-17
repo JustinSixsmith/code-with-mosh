5ed8d8fe2ddc3da9ce720be2224e81eed313da3d
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-17 15:10:16 -0600

    shows difference between imperative and declarative programming

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -2,9 +2,10 @@ package com.codewithmosh;
 
 import com.codewithmosh.collections.*;
 import com.codewithmosh.lambdas.LambdasDemo;
+import com.codewithmosh.streams.StreamsDemo;
 
 public class Main {
     public static void main(String[] args) {
-        LambdasDemo.show();
+        StreamsDemo.show();
     }
 }
--- src/com/codewithmosh/streams/StreamsDemo.java
+++ src/com/codewithmosh/streams/StreamsDemo.java
@@ -1,6 +1,25 @@
 package com.codewithmosh.streams;
 
+import java.util.List;
+
 public class StreamsDemo {
-  public static void show() {
-  }
+    public static void show() {
+        List<Movie> movies = List.of(
+                new Movie("a", 10),
+                new Movie("b", 15),
+                new Movie("b", 20)
+        );
+
+        // Imperative Programming
+        int count = 0;
+        for (var movie : movies) {
+            if (movie.getLikes() > 10)
+                count++;
+        }
+
+        // Declarative (Functional) Programming
+        var count2 = movies.stream()
+                .filter(movie -> movie.getLikes() > 10)
+                .count();
+    }
 }
