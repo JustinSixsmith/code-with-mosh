89dfdc6a1011b8d0ed13cbf1cc0084c22690d003
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-17 14:41:11 -0600

    works with UnaryOperator

--- src/com/codewithmosh/lambdas/LambdasDemo.java
+++ src/com/codewithmosh/lambdas/LambdasDemo.java
@@ -3,12 +3,14 @@ package com.codewithmosh.lambdas;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 public class LambdasDemo {
     public static void show() {
-        BinaryOperator<Integer> add = (a, b) -> a + b;
-        Function<Integer, Integer> square = a -> a * a;
-        var result = add.andThen(square).apply(1, 2);
+        UnaryOperator<Integer> square = n -> n * n;
+        UnaryOperator<Integer> increment = n -> n + 1;
+
+        var result = increment.andThen(square).apply(1);
         System.out.println(result);
     }
 }
