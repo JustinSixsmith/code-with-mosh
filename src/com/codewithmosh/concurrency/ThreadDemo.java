173a52e428d3a04b8403d69983a69954f79bd27e
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-18 12:48:54 -0600

    joins tasks in a new thread

--- src/com/codewithmosh/concurrency/ThreadDemo.java
+++ src/com/codewithmosh/concurrency/ThreadDemo.java
@@ -2,11 +2,16 @@ package com.codewithmosh.concurrency;
 
 public class ThreadDemo {
   public static void show() {
-    System.out.println(Thread.currentThread().getName());
-
-    for (var i = 0; i < 10; i++) {
-      Thread thread = new Thread(new DownloadFileTask());
+    Thread thread = new Thread(new DownloadFileTask());
       thread.start();
-    }
+
+      try {
+        thread.join();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+
+      System.out.println("File is ready to be scanned.");
+
   }
 }
