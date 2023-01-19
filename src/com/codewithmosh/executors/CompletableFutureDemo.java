071f737b1093107d957b1fadcadfb0fd184ea858
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-19 12:21:56 -0600

    creates a new completable future in case an exception is thrown

--- src/com/codewithmosh/executors/CompletableFutureDemo.java
+++ src/com/codewithmosh/executors/CompletableFutureDemo.java
@@ -7,18 +7,32 @@ import java.util.function.Supplier;
 
 public class CompletableFutureDemo {
     public static void show() {
-        var future = CompletableFuture.supplyAsync(() -> 1);
-        future.thenAcceptAsync(result -> {
-            System.out.println(Thread.currentThread().getName());
-            System.out.println(result);
+        var future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Getting the current weather");
+            throw new IllegalStateException();
         });
 
         try {
-            Thread.sleep(2000);
+            var temperature = future.exceptionally(ex -> 1).get();
+            System.out.println(temperature);
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
             e.printStackTrace();
         }
 
+
+//        var future = CompletableFuture.supplyAsync(() -> 1);
+//        future.thenAcceptAsync(result -> {
+//            System.out.println(Thread.currentThread().getName());
+//            System.out.println(result);
+//        });
+//
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
 //        Supplier<Integer> task = () -> 1;
 //        var future = CompletableFuture.supplyAsync(task);
