c6981c5d54ccdccc0d8ce4b3e962b3e7c6738b87
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-19 13:59:21 -0600

    builds asynchronous future in a declarative way

--- src/com/codewithmosh/executors/CompletableFutureDemo.java
+++ src/com/codewithmosh/executors/CompletableFutureDemo.java
@@ -6,21 +6,30 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.function.Supplier;
 
 public class CompletableFutureDemo {
+    public static int toFahrenheit(int celsius) {
+        return (int) (celsius * 1.8) + 32;
+    }
+
     public static void show() {
-        var future = CompletableFuture.supplyAsync(() -> {
-            System.out.println("Getting the current weather");
-            throw new IllegalStateException();
-        });
+        var future = CompletableFuture.supplyAsync(() -> 20);
+        future
+            .thenApply(CompletableFutureDemo::toFahrenheit)
+            .thenAccept(System.out::println);
 
-        try {
-            var temperature = future.exceptionally(ex -> 1).get();
-            System.out.println(temperature);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
 
+//        var future = CompletableFuture.supplyAsync(() -> {
+//            System.out.println("Getting the current weather");
+//            throw new IllegalStateException();
+//        });
+//
+//        try {
+//            var temperature = future.exceptionally(ex -> 1).get();
+//            System.out.println(temperature);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
 
 //        var future = CompletableFuture.supplyAsync(() -> 1);
 //        future.thenAcceptAsync(result -> {
