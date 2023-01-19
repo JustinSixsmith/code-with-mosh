8334fcfa609aa6c75afa675643d34ca6a234b0a5
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-19 11:26:22 -0600

    wraps send mail service in a CompletableFuture object

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,10 +1,19 @@
 package com.codewithmosh;
 
 import com.codewithmosh.executors.CompletableFutureDemo;
+import com.codewithmosh.executors.MailService;
 
 public class Main {
     public static void main(String[] args) {
-        CompletableFutureDemo.show();
+        var service = new MailService();
+        service.sendAsync();
+        System.out.println("Hello World");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
 //        System.out.println(Thread.activeCount());
 //        System.out.println(Runtime.getRuntime().availableProcessors());
